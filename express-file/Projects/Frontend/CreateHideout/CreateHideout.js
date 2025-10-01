244dde54b4de8da77376cad6174da13fec63c0c7
Kyarasu0 <Setsuru.lilac@gmail.com>
2025-10-02 03:25:02 +0900

    CreateHideout-modal: InProgress
    Path: Projects/Frontend/CreateHideout-modal
    Description: 写真を数件指定してpreviewで一枚一枚見ながら都度埋め込みとDownloadをしていく形式です。

--- express-file/Projects/Frontend/CreateHideout/CreateHideout.js
+++ express-file/Projects/Frontend/CreateHideout/CreateHideout.js
@@ -1,8 +1,16 @@
 const uploadButton = document.getElementById("uploadButton");
 const fileInput = document.getElementById("fileInput");
 const fileCount = document.getElementById("fileCount");
+// modal設定
+const preview = document.getElementById("preview");
+const confirm = document.getElementById("confirm");
+const closeOverlay = document.getElementById("closeOverlay");
+const overlay = document.getElementById("overlay");
 
+// upload buttonの設定
 uploadButton.addEventListener("click", () => {fileInput.click()});
+
+// fileInputの設定
 fileInput.addEventListener("change", () => {
     if (fileInput.files.length < 1000){
         const count = fileInput.files.length;
@@ -10,6 +18,26 @@ fileInput.addEventListener("change", () => {
     }else{
         alert("ファイル数は999個まででお願いします！");
     }
+});
+
+// confirmの設定
+confirm.addEventListener("click", () => {
+    // 画面の初期化
+    preview.innerHTML = "";
+
+    Array.from(fileInput.files).forEach(file => {
+        const img = document.createElement("img");
+        img.src = URL.createObjectURL(file);
+        preview.appendChild(img);
+    });
+
+    overlay.style.display = "flex";
+
+});
+
+// closeOverlayの設定
+closeOverlay.addEventListener("click", () => {
+    overlay.style.display = "none";
 });
 
 
